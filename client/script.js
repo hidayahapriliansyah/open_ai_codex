37203c806e3b5d4787634044d06c7a898493216f
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-22 09:08:24 +0700

    chat gpt front end

---
+++ client/script.js
@@ -0,0 +1,86 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+
+// loader waiting ai to reply
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if(element.textContent === '...') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+// text typing effect
+function typeText(element, text) {
+  let index = 0;
+
+  let loadInterval = setInterval(() => {
+    if(index < text.length) {
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(loadInterval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16).slice(2);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+          <div class="profile">
+            <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}" />
+          </div>
+          <div class="message" id="${uniqueId}">${value}</div>
+        </div>
+      </div>
+    `
+  );
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault;
+
+  const data = new FormData(form);
+
+  // user's chatsripe 
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  // bot's chartstipe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if(e.key = 13) {
+    handleSubmit(e);
+  }
+});
